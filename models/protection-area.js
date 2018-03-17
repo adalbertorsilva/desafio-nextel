1b917f89ae3d779506366f7c8ae9aefe3ccbabec
adalbertorsilva <adalbertorsilvajr@gmail.com>
2018-03-17 15:47:11 -0300

    - added docs to protection area exposed methods - removed npm start from npm run build command

--- models/protection-area.js
+++ models/protection-area.js
@@ -1,6 +1,12 @@
 'use strict'
 const {Model, DataTypes} = require('sequelize')
 
+/**
+ * class that represents the
+ * protection area entity
+ * 
+ * @class
+ */
 module.exports = (sequelize) => {
   class ProtectionArea extends Model {
     static init (sequelize) {
@@ -12,6 +18,9 @@ module.exports = (sequelize) => {
       }, {sequelize, underscored: true})
     }
 
+    /**
+     * creates an object to be sent as response
+     */
     responseObject () {
       const responseObject = {
         id: this.id,
@@ -27,6 +36,11 @@ module.exports = (sequelize) => {
       this.belongsTo(models.SuperHero, {as: 'hero', foreignKey: 'super_hero_id'})
     }
 
+    /**
+     * retrieves all protection areas in a 10km radius
+     * 
+     * @param coordinates - object that contains latitude and longitude references
+     */
     static async findNotifiedAreas(coordinates) {
       return await sequelize.query('SELECT * FROM "ProtectionAreas" WHERE ST_DWithin(point::geography, ST_MakePoint(:longitude,:latitude)::geography, 10000) limit 8', { replacements: { longitude: coordinates.longitude, latitude: coordinates.latitude }, model: ProtectionArea})
     }
